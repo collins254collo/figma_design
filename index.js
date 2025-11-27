bfa29ba6ec397368c6418713716dd71cff3f765e
Collins Njogu <njogucollins10397@gmail.com>
2025-11-27 11:39:35 +0300

    removing the commits message

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 // Get the "Select All" checkbox
 const selectAll = document.getElementById("selectAll");
 
-// Listen for changes on the select-all checkbox
 selectAll.addEventListener("change", function() {
-  // Get all checkboxes with the class 'check'
+  
   const allCheckboxes = document.querySelectorAll(".check");
 
   allCheckboxes.forEach((checkbox) => {
